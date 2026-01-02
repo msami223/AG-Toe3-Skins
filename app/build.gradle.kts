05f38b3f8b31a20be52e4251a5bdc13fcd065cac
msami223 <dekinswite@gmail.com>
2026-01-02 21:58:09 +0500

    Hotfix v1.4: Disable R8 to fix crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.devstormtech.toe3skins"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -30,7 +30,7 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true // Enable R8 shrinking to reduce size
+            isMinifyEnabled = false // Disabled R8 to fix crash
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
